14056dfc02ef785981de3533ba01ca2eae535f29
ewelton <eric@korsimoro.com>
2019-12-01 19:02:54 +0700

    testing react-site-sync

--- react-site/static.config.js
+++ react-site/static.config.js
@@ -6,33 +6,11 @@ import path from 'path'
 
 export default {
   entry: path.join(__dirname, 'src', 'index.tsx'),
-  siteRoot: "https://ewelton.github.io",
+  siteRoot: "https://ewelton.github.io/ktest/site",
   basePath: "ktest/site",
   getRoutes: async () => {
       return []
     },
-  /*
-  getRoutes: async () => {
-    const { data: posts } /* :{ data: Post[] } * / = await axios.get(
-      'https://jsonplaceholder.typicode.com/posts'
-    )
-    return [
-      {
-        path: '/blog',
-        getData: () => ({
-          posts,
-        }),
-        children: posts.map((post /* : Post * /) => ({
-          path: `/post/${post.id}`,
-          template: 'src/containers/Post',
-          getData: () => ({
-            post,
-          }),
-        })),
-      },
-    ]
-  },
-  */
   plugins: [
     'react-static-plugin-typescript',
     [
